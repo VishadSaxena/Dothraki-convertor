41ff9241ddfa63a8094e5d398ce8c3d6a27c9783
Vishad Saxena <vishad1710@gmail.com>
2023-01-09 00:40:14 +0530

    updated the app

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let textArea = document.querySelector("textarea");
+let textArea = document.querySelector("#textarea");
 let transButton = document.querySelector("#tButton");
 let outputText = document.querySelector("#output");
 
@@ -22,3 +22,40 @@ transButton.addEventListener("click", function clickEventHandler() {
         }).catch(errorHandler)
 });
 
+/*
+var btnTranslate = document.querySelector("#tButton");
+
+var txtInput = document.querySelector("textarea");
+
+var outputDiv = document.querySelector("#output");
+
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverURL = "https://api.funtranslations.com/translate/dothraki.json";
+
+function gerTranslationURL(text){
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("Something wrong with the server, try again after sometime");
+}
+
+function clickEventHandler(){
+    //outputDiv.innerText = "ajsajsajsajas " + txtInput.value;
+
+    var inputText = txtInput.value; //Taking input
+
+    //calling server
+    fetch(gerTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", clickEventHandler)
+*/
